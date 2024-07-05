82d57eea197a153e6aa8d8c04aabf1e82fb93818
Angelika Stefan <anmaste92@gmail.com>
2024-07-05 16:21:39 +0200

    update exploratory

--- exploratory_MoC_development.R
+++ exploratory_MoC_development.R
@@ -13,6 +13,7 @@ library(reshape2)
 library(brms)
 library(blavaan)
 library(logspline)
+library(nlme)
 
 ############################ LOAD MODEL FITS ###################################
 
@@ -66,21 +67,21 @@ ESdata_long$treat <- as.factor(ESdata_long$treat)
 colnames(ESdata_long) <- c("ID", "treat", "time", "ESscore")
 
 # Frequentist regression models
-lm_es_noInt <- summary(lm(ESscore ~ treat + time, data = ESdata_long))
-lm_es_withInt <- summary(lm(ESscore ~ treat * time, data = ESdata_long))
+lm_es_noInt <- summary(lme(ESscore ~ treat + time, data = ESdata_long[!is.na(ESdata_long$ESscore),], random=~1|ID, method="REML"))
+lm_es_withInt <- summary(lme(ESscore ~ treat * time, data = ESdata_long[!is.na(ESdata_long$ESscore),], random=~1|ID, method="REML"))
 
 hyps_Interaction <- c("treatST = 0", "timees_2 = 0", "timees_3 = 0", "timees_4 = 0", "treatST:timees_2 = 0", "treatST:timees_3 = 0", "treatST:timees_4 = 0")
 hyps_noInteraction <- c("treatST = 0", "timees_2 = 0", "timees_3 = 0", "timees_4 = 0")
 
 # Bayesian regression models
-bayesian_es_noInteraction <- brm(ESscore ~ treat + time, data = na.omit(ESdata_long), iter=10000, sample_prior="yes", prior=prior)
-bayesian_es_Interaction <- brm(ESscore ~ treat * time, data = na.omit(ESdata_long), iter=10000, sample_prior="yes", prior=prior)
+bayesian_es_noInteraction <- summary(brm(ESscore ~ treat + time + (1|ID), data = na.omit(ESdata_long), iter=10000, sample_prior="yes", prior=prior))
+bayesian_es_Interaction <- summary(brm(ESscore ~ treat * time + (1|ID), data = na.omit(ESdata_long), iter=10000, sample_prior="yes", prior=prior))
 
 bf_es_noInt <- hypothesis(bayesian_es_noInteraction, hyps_noInteraction)
 bf_es_withInt <- hypothesis(bayesian_es_Interaction, hyps_Interaction)
 
-coef_tests_ES_noInt <- round(cbind(coefficients(lm_es_noInt), BF01 = c(NA, bf_es_noInt$hypothesis$Evid.Ratio)), 3)
-coef_tests_ES_Int <- round(cbind(coefficients(lm_es_withInt), BF01 = c(NA, bf_es_withInt$hypothesis$Evid.Ratio)), 3)
+coef_tests_ES_noInt <- round(cbind(coef(lm_es_noInt), BF10 = c(NA, 1/bf_es_noInt$hypothesis$Evid.Ratio)), 3)
+coef_tests_ES_Int <- round(cbind(coef(lm_es_withInt), BF10 = c(NA, 1/bf_es_withInt$hypothesis$Evid.Ratio)), 3)
 
 save(coef_tests_ES_noInt, file="../exploratory/coef_tests_ES_noInt.RData")
 save(coef_tests_ES_Int, file="../exploratory/coef_tests_ES_Int.RData")
@@ -108,20 +109,20 @@ CRdata_long$variable <- as.factor(CRdata_long$variable)
 CRdata_long$treat <- as.factor(CRdata_long$treat)
 colnames(CRdata_long) <- c("ID", "treat", "time", "CRscore")
 
-lm_cr_noInt <- summary(lm(CRscore ~ treat + time, data = CRdata_long))
-lm_cr_withInt <- summary(lm(CRscore ~ treat * time, data = CRdata_long))
+lm_cr_noInt <- summary(lme(CRscore ~ treat + time, data = CRdata_long[!is.na(CRdata_long$CRscore),], random=~1|ID, method="REML"))
+lm_cr_withInt <- summary(lme(CRscore ~ treat * time, data = CRdata_long[!is.na(CRdata_long$CRscore),], random=~1|ID, method="REML"))
 
 hyps_Interaction <- c("treatST = 0", "timecr_2 = 0", "timecr_3 = 0", "timecr_4 = 0", "treatST:timecr_2 = 0", "treatST:timecr_3 = 0", "treatST:timecr_4 = 0")
 hyps_noInteraction <- c("treatST = 0", "timecr_2 = 0", "timecr_3 = 0", "timecr_4 = 0")
 
-bayesian_cr_noInteraction <- brm(CRscore ~ treat + time, data = na.omit(CRdata_long), iter=10000, sample_prior="yes", prior=prior)
-bayesian_cr_Interaction <- brm(CRscore ~ treat * time, data = na.omit(CRdata_long), iter=10000, sample_prior="yes", prior=prior)
+bayesian_cr_noInteraction <- brm(CRscore ~ treat + time + (1|ID), data = na.omit(CRdata_long), iter=10000, sample_prior="yes", prior=prior)
+bayesian_cr_Interaction <- brm(CRscore ~ treat * time + (1|ID), data = na.omit(CRdata_long), iter=10000, sample_prior="yes", prior=prior)
 
 bf_cr_noInt <- hypothesis(bayesian_cr_noInteraction, hyps_noInteraction)
 bf_cr_withInt <- hypothesis(bayesian_cr_Interaction, hyps_Interaction)
 
-coef_tests_CR_noInt <- round(cbind(coefficients(lm_cr_noInt), BF01 = c(NA, bf_cr_noInt$hypothesis$Evid.Ratio)), 3)
-coef_tests_CR_Int <- round(cbind(coefficients(lm_cr_withInt), BF01 = c(NA, bf_cr_withInt$hypothesis$Evid.Ratio)), 3)
+coef_tests_CR_noInt <- round(cbind(coef(lm_cr_noInt), BF10 = c(NA, 1/bf_cr_noInt$hypothesis$Evid.Ratio)), 3)
+coef_tests_CR_Int <- round(cbind(coefficients(lm_cr_withInt), BF10 = c(NA, 1/bf_cr_withInt$hypothesis$Evid.Ratio)), 3)
 
 save(coef_tests_CR_noInt, file="../exploratory/coef_tests_CR_noInt.RData")
 save(coef_tests_CR_Int, file="../exploratory/coef_tests_CR_Int.RData")
@@ -149,20 +150,20 @@ AXdata_long$variable <- as.factor(AXdata_long$variable)
 AXdata_long$treat <- as.factor(AXdata_long$treat)
 colnames(AXdata_long) <- c("ID", "treat", "time", "AXscore")
 
-lm_ax_noInt <- summary(lm(AXscore ~ treat + time, data = AXdata_long))
-lm_ax_withInt <- summary(lm(AXscore ~ treat * time, data = AXdata_long))
+lm_ax_noInt <- summary(lme(AXscore ~ treat + time, data = AXdata_long[!is.na(AXdata_long$AXscore),], random=~1|ID, method="REML"))
+lm_ax_withInt <- summary(lme(AXscore ~ treat * time, data = AXdata_long[!is.na(AXdata_long$AXscore),], random=~1|ID, method="REML"))
 
 hyps_Interaction <- c("treatST = 0", "timeax_3 = 0", "timeax_4 = 0", "treatST:timeax_3 = 0", "treatST:timeax_4 = 0")
 hyps_noInteraction <- c("treatST = 0", "timeax_3 = 0", "timeax_4 = 0")
 
-bayesian_ax_noInteraction <- brm(AXscore ~ treat + time, data = na.omit(AXdata_long), iter=10000, sample_prior="yes", prior=prior)
-bayesian_ax_Interaction <- brm(AXscore ~ treat * time, data = na.omit(AXdata_long), iter=10000, sample_prior="yes", prior=prior)
+bayesian_ax_noInteraction <- brm(AXscore ~ treat + time + (1|ID), data = na.omit(AXdata_long), iter=10000, sample_prior="yes", prior=prior)
+bayesian_ax_Interaction <- brm(AXscore ~ treat * time + (1|ID), data = na.omit(AXdata_long), iter=10000, sample_prior="yes", prior=prior)
 
 bf_ax_noInt <- hypothesis(bayesian_ax_noInteraction, hyps_noInteraction)
 bf_ax_withInt <- hypothesis(bayesian_ax_Interaction, hyps_Interaction)
 
-coef_tests_AX_noInt <- round(cbind(coefficients(lm_ax_noInt), BF01 = c(NA, bf_ax_noInt$hypothesis$Evid.Ratio)), 3)
-coef_tests_AX_Int <- round(cbind(coefficients(lm_ax_withInt), BF01 = c(NA, bf_ax_withInt$hypothesis$Evid.Ratio)), 3)
+coef_tests_AX_noInt <- round(cbind(coef(lm_ax_noInt), BF10 = c(NA, 1/bf_ax_noInt$hypothesis$Evid.Ratio)), 3)
+coef_tests_AX_Int <- round(cbind(coef(lm_ax_withInt), BF10 = c(NA, 1/bf_ax_withInt$hypothesis$Evid.Ratio)), 3)
 
 save(coef_tests_AX_noInt, file="../exploratory/coef_tests_AX_noInt.RData")
 save(coef_tests_AX_Int, file="../exploratory/coef_tests_AX_Int.RData")
@@ -187,20 +188,20 @@ AVdata_long$variable <- as.factor(AVdata_long$variable)
 AVdata_long$treat <- as.factor(AVdata_long$treat)
 colnames(AVdata_long) <- c("ID", "treat", "time", "AVscore")
 
-lm_av_noInt <- summary(lm(AVscore ~ treat + time, data = AVdata_long))
-lm_av_withInt <- summary(lm(AVscore ~ treat * time, data = AVdata_long))
+lm_av_noInt <- summary(lme(AVscore ~ treat + time, data = AVdata_long[!is.na(AVdata_long$AVscore),], random=~1|ID, method="REML"))
+lm_av_withInt <- summary(lme(AVscore ~ treat * time, data = AVdata_long[!is.na(AVdata_long$AVscore),], random=~1|ID, method="REML"))
 
 hyps_Interaction <- c("treatST = 0", "timeav_3 = 0", "timeav_4 = 0", "treatST:timeav_3 = 0", "treatST:timeav_4 = 0")
 hyps_noInteraction <- c("treatST = 0", "timeav_3 = 0", "timeav_4 = 0")
 
-bayesian_av_noInteraction <- brm(AVscore ~ treat + time, data = na.omit(AVdata_long), iter=10000, sample_prior="yes", prior=prior)
-bayesian_av_Interaction <- brm(AVscore ~ treat * time, data = na.omit(AVdata_long), iter=10000, sample_prior="yes", prior=prior)
+bayesian_av_noInteraction <- brm(AVscore ~ treat + time + (1|ID), data = na.omit(AVdata_long), iter=10000, sample_prior="yes", prior=prior)
+bayesian_av_Interaction <- brm(AVscore ~ treat * time + (1|ID), data = na.omit(AVdata_long), iter=10000, sample_prior="yes", prior=prior)
 
 bf_av_noInt <- hypothesis(bayesian_av_noInteraction, hyps_noInteraction)
 bf_av_withInt <- hypothesis(bayesian_av_Interaction, hyps_Interaction)
 
-coef_tests_AV_noInt <- round(cbind(coefficients(lm_av_noInt), BF01 = c(NA, bf_av_noInt$hypothesis$Evid.Ratio)), 3)
-coef_tests_AV_Int <- round(cbind(coefficients(lm_av_withInt), BF01 = c(NA, bf_av_withInt$hypothesis$Evid.Ratio)), 3)
+coef_tests_AV_noInt <- round(cbind(coef(lm_av_noInt), BF10 = c(NA, 1/bf_av_noInt$hypothesis$Evid.Ratio)), 3)
+coef_tests_AV_Int <- round(cbind(coef(lm_av_withInt), BF10 = c(NA, 1/bf_av_withInt$hypothesis$Evid.Ratio)), 3)
 
 save(coef_tests_AV_noInt, file="../exploratory/coef_tests_AV_noInt.RData")
 save(coef_tests_AV_Int, file="../exploratory/coef_tests_AV_Int.RData")
